40f583eee730e78b61e83c657e846f1f85003c58
vignesh <vignesh@revaadigital.com>
2025-03-15 05:11:34 +0530

    second commit

--- landing_project/settings.py
+++ landing_project/settings.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = 'landing_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'landing_project/templates')], 
+        'DIRS': [os.path.join(BASE_DIR, 'templates')], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
